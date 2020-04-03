eee857714bb04a991d664227e38270e71689613a
Andrew Baxter <andrewbaxter439@gmail.com>
2020-04-03 09:55:00 +0100

    adding skeleton walk-thorugh app

---
+++ Example/ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Examples of inputs and outputs"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput(inputId = "bins",
+                        label = "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30),
+            
+            textInput(inputId = "gtitle",
+                      label = "Graph title"),
+            
+            checkboxInput(inputId = "checks",
+                          label = "Check boxes")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot")
+        )
+    )
+))
