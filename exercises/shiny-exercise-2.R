49ccfc89df8b407969748926de7af44cbe328900
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-05-01 08:59:43 +0100

    to match script

--- exercises/shiny-exercise-2.R
+++ exercises/shiny-exercise-2.R
@@ -2,7 +2,9 @@
 ###############################################.
 ## Global ----
 ###############################################.
-source("packages.R") # load packages
+library(shiny)
+library(ggplot2)
+library(dplyr)
 
 # Reading in the data
 rii_sii_ethnicity <- read_csv("data/RII_SII_by_ethnicity_age_dep_2011.csv")
@@ -10,7 +12,7 @@ st_rates_ethnicity <- read_csv("data/st_rates_by_ethnicity_age_dep_2011.csv")
 
 age_grps <- unique(st_rates_ethnicity$age)
 depr_grps <- unique(st_rates_ethnicity$ses)
-
+ethnicity_list <- unique(st_rates_ethnicity$ethnicity)
 
 ###############################################.
 ## UI ----
@@ -18,8 +20,9 @@ depr_grps <- unique(st_rates_ethnicity$ses)
 ui <- fluidPage("Scotland Test", 
                 h3("Hello World!"),
                 br(),
-                selectInput("age", "Select age group", choices = age_grps),
-                selectInput("depr", "Select deprivation group", choices = depr_grps),
+                selectInput("ethnicity", label = "Select an ethnicity", choices= ethnicity_list),
+                # selectInput("age", "Select age group", choices = age_grps),
+                # selectInput("depr", "Select deprivation group", choices = depr_grps),
                 plotOutput("barplot")
                 
 ) # fluidPage bracket
@@ -33,12 +36,12 @@ server <- function(input, output, session) {
   # Filtering data based on user input
 st_rates_filtered <- reactive({
   st_rates_ethnicity %>% 
-    filter(age == input$age &
-             ses == input$depr)
+    filter(ethnicity == input$ethnicity)# &
+             # ethnicity == input$depr)
 })
 
   output$barplot <- renderPlot({
-    ggplot(st_rates_filtered(), aes(x = ethnicity, y = rate)) +
+    ggplot(st_rates_filtered(), aes(x = age, y = rate)) +
       geom_bar(stat = "identity")
   })
   
